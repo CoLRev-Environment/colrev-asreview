6dda0b870274252f14b4a280ba417522744a5f48
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2023-07-05 15:36:32 +0200

    import conftest from colrev

--- tests/asreview_test.py
+++ tests/asreview_test.py
@@ -1,2 +1,19 @@
-def test_asreview() -> None:
-    assert True
+#! /usr/bin/env python
+"""Tests for colrev-asreview"""
+
+import colrev.review_manager
+
+import colrev_asreview.colrev_asreview
+
+
+def test_asreview(
+    base_repo_review_manager: colrev.review_manager.ReviewManager,
+) -> None:
+    """Test colrev-asreview"""
+    prescreen_operation = base_repo_review_manager.get_prescreen_operation()
+
+    settings = {"endpoint": "colrev_asreview.colrev_asreview"}
+    asreview = colrev_asreview.colrev_asreview.ASReviewPrescreen(
+        prescreen_operation=prescreen_operation, settings=settings
+    )
+    assert asreview.settings.endpoint == "colrev_asreview.colrev_asreview"
---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+"""Conftest for colrev-asreview"""
+
+from colrev.testutils.conftest import *
